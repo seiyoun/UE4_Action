22e804601f29b8aa7f4cc3bd38998f6773d019de
seiyoun <shiyuan0106@gmail.com>
2021-09-06 20:40:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestCharacterProject
+
+Developed with Unreal Engine 4
---
+++ Source/TestCharacterProject/TestCharacterProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestCharacterProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCHARACTERPROJECT_API ATestCharacterProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
